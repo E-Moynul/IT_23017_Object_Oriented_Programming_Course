c72deb408b2a5883bbe567f37bab40281460d222
E-Moynul <it23017@mbstu.ac.bd>
2024-11-18 00:20:01 +0600

    Create ch1ex7_it23017.java

---
+++ Exercises_from_liang's_book/ch1ex7_it23017.java
@@ -0,0 +1,13 @@
+public class ch1ex7_it23017 {
+	public static void main(String[] args) {
+		
+		double Pi = 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11);
+		double Pi2 = 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13);
+
+		System.out.println(Pi);
+		System.out.println(Pi2);
+		System.out.println(Math.PI);
+	}
+	
+
+}
