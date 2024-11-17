44d5fa0405568415c9732425d6c152663147bd94
E-Moynul <it23017@mbstu.ac.bd>
2024-11-18 00:17:10 +0600

    Create ch1ex4_it23017.java

---
+++ Exercises_from_liang's_book/ch1ex4_it23017.java
@@ -0,0 +1,11 @@
+public class ch1ex4_it23017 {
+
+    public static void main(String[] args) {
+        int num = 4;
+        System.out.println("a a^2 a^3");
+        for (int row = 1; row <= num; row++) {
+            System.out.println(row + " " + row * row + " " + row * row * row);
+        }
+    }
+
+}
