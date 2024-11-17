38a30eb60c0e7d1cb90dc260ca51b418cb5edb15
E-Moynul <it23017@mbstu.ac.bd>
2024-11-18 00:20:51 +0600

    Create ch1ex8_it23017.java

---
+++ Exercises_from_liang's_book/ch1ex8_it23017.java
@@ -0,0 +1,12 @@
+public class ch1ex8_it23017 {
+    public static void main(String[] args) {
+
+        double radius = 5.5;
+        double perimeter = 2 * radius * Math.PI;
+        double area = radius * radius * Math.PI;
+
+        System.out.println("Area of a circle with radius 5.5 is: " + area);
+        System.out.println("Perimeter of a circle with radius 5.5 is: " + perimeter);
+
+    }
+}
