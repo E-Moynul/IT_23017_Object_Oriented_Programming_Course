526a38cbaf6bc58862a921387dac6ee0e9ee30a3
E-Moynul <it23017@mbstu.ac.bd>
2024-11-18 00:18:17 +0600

    Create ch1ex5_it23017.java

---
+++ Exercises_from_liang's_book/ch1ex5_it23017.java
@@ -0,0 +1,13 @@
+public class ch1ex5_it23017 {
+    public static void main(String[] args) {
+        double numerator = (9.5 * 4.5) - (2.5 * 3);
+        double denominator = 45.5 - 3.5;
+
+        double solution = numerator / denominator;
+
+        String outStr = solution + " ";
+
+        System.out.printf("%.4f", solution);
+
+    }
+}
