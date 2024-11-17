b8dc0cabb87708fd212a78084a3593979fc65c90
E-Moynul <it23017@mbstu.ac.bd>
2024-11-18 00:13:27 +0600

    Create ch1ex2_it23017.java

---
+++ Exercises_from_liang's_book/ch1ex2_it23017.java
@@ -0,0 +1,12 @@
+public class ch1ex2 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            System.out.print("Welcome to Java\n");
+            count++;
+
+        }
+    }
+
+}
